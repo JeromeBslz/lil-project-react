94a30f36473c1b7c7c08616a569b8a024a1ed264
ileanaspl <ileana.saintpaul@gmail.com>
2024-04-02 11:30:38 +0200

    create NavBar component

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,21 @@
+function NavBar() {
+	return (
+		<>
+			<nav>
+				<ul>
+					<li>
+						<a href="">Classes</a>
+					</li>
+					<li>
+						<a href="">Equipement</a>
+					</li>
+					<li>
+						<a href="">Boss</a>
+					</li>
+				</ul>
+			</nav>
+		</>
+	);
+}
+
+export default NavBar;
