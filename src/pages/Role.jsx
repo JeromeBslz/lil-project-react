7ebf249bb24e9923cacbd6f7c24d44158e2d1dee
ileanaspl <ileana.saintpaul@gmail.com>
2024-04-03 11:23:46 +0200

    add stats for classes

--- src/pages/Role.jsx
+++ src/pages/Role.jsx
@@ -2,26 +2,37 @@ import roleArray from '../role';
 import './Role.css';
 
 function Role() {
-    return (
-        <>
-            <h2 className='role-name'>Les classes dans Elden Ring</h2>
-            <div className='grid'>
-                {roleArray.map((role, index) => (
-                    <div key={index} className='role-card'>
-                        <h3>{role.name}</h3>
-                        <p>{role.text}</p>
-                        <img src={role.image} alt={role.name} />
-                    </div>
-                ))}
-            </div>
-        </>
-    );
+	return (
+		<>
+			<h2 className="role-name">Les classes</h2>
+			<div className="grid">
+				{roleArray.map((role, index) => (
+					<div key={index} className="role-card">
+						<h3>{role.name}</h3>
+						<p>{role.text}</p>
+						<div className="image-and-stats">
+							<img src={role.image} alt={role.name} />
+							<ul>
+								<li>Stats de base :</li>
+								<li>{role.niveau}</li>
+								<li>{role.vigueur}</li>
+								<li>{role.esprit}</li>
+								<li>{role.endurance}</li>
+								<li>{role.force}</li>
+								<li>{role.dextérité}</li>
+								<li>{role.intelligence}</li>
+								<li>{role.foi}</li>
+								<li>{role.esotérisme}</li>
+							</ul>
+						</div>
+					</div>
+				))}
+			</div>
+		</>
+	);
 }
 
 export default Role;
-
-
-
 
 // import classArray from '../class';
 
@@ -36,9 +47,7 @@ export default Role;
 // 					image={item.image}
 // 				/>
 
-// 			))}			
+// 			))}
 // 		</div>
 // 	);
-// }	
-
-
+// }
